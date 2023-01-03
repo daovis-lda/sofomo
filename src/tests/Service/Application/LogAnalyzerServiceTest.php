4fe8285433db62bf1e0f6a20f63399aa5d84d4dd
daovis-lda <99510666+daovis-lda@users.noreply.github.com>
2023-01-03 12:46:33 +0100

    Update LogAnalyzerServiceTest.php

--- src/tests/Service/Application/LogAnalyzerServiceTest.php
+++ src/tests/Service/Application/LogAnalyzerServiceTest.php
@@ -17,4 +17,29 @@ class LogAnalyzerServiceTest extends KernelTestCase
 
         $this->assertTrue($result);
     }
+    
+    //another example of test
+    public function testCleanupLogs(): void
+    {
+        // test log file with some old and some new records
+        $logFile = 'test.log';
+        file_put_contents($logFile, "2022-01-01 00:00:00 This is a new record\n");
+        file_put_contents($logFile, "2021-01-01 00:00:00 This is an old record\n", FILE_APPEND);
+        file_put_contents($logFile, "2022-01-01 00:00:00 This is a new record\n", FILE_APPEND);
+
+        // Test removing records older than 1 day
+        $logAnalyzerRequest = new LogAnalyzerRequest($logFile, 1);
+        $logService = new LogAnalyzerService();
+        $result = $logService->execute($logAnalyzerRequest);
+
+        $lines = file($logFile);
+        $this->assertEquals(count($lines), 2);
+        $this->assertEquals($lines[0], "2022-01-01 00:00:00 This is a new record\n");
+        $this->assertEquals($lines[1], "2022-01-01 00:00:00 This is a new record\n");
+
+        // Test removing records older than 365 days
+        $logAnalyzerRequest = new LogAnalyzerRequest($logFile, 365);
+        $lines = file($logFile);
+        $this->assertEquals(count($lines), 0);
+    }
 }
